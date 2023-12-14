652a2a205651cf81bd70383daada8560d2bef9ef
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2023-12-14 15:31:55 +0300

    Add `Noto Sans Ethiopic` font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-        colors: {
-            'primary': '#2463eb',
-            'secondary': '#0C121C',
-        },
-        fontFamily: {
-            'sans': ['Inter'],
-            'lexend': ['Lexend'],
-        }
+      colors: {
+        primary: "#2463eb",
+        secondary: "#0C121C",
+      },
+      fontFamily: {
+        sans: ["Inter"],
+        lexend: ["Lexend"],
+        basic: ["Lexend", "Noto Sans Ethiopic"],
+      },
     },
   },
   plugins: [],
-}
+};
