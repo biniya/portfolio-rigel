8355f657f1e070d907a29ebddd4ec2c1ccb5b25f
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-05-02 08:51:22 +0300

    Update Hero UI

---
+++ src/components/GridBackground.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="absolute top-0 -z-50 h-screen mx-auto w-full">
+    <div class="flex w-full h-full">
+      <div class="gradient-mask flex flex-wrap w-11/12 opacity-[0.03] border">
+        <div
+          v-for="index in 850"
+          :key="index"
+          class="aspect-square w-20 h-20 border border-black"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.gradient-mask {
+  mask-image: conic-gradient(rgba(0, 0, 0, 1) 0%, transparent 95%);
+}
+</style>
--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -1,98 +1,87 @@
 <template>
-  <section
-    class="mx-auto bg-image items-center flex flex-col justify-between bg-black space-y-5 min-h-screen h-screen pt-12 text-center"
-  >
-    <div
-      class="w-11/12 h-full py-3 items-center md:flex-row flex-col space-y-3 md:space-y-0 flex justify-evenly"
+  <section class="flex justify-between w-10/12 mx-auto">
+    <GridBackground />
+    <section
+      class="w-7/12 items-center flex flex-col space-y-5 justify-evenly min-h-screen h-screen pt-12"
     >
-      <!--          Left section-->
       <div
-        class="w-full md:w-7/12 flex flex-col space-y-5 md:space-y-0 items-center justify-evenly min-h-full"
+        class="w-full h-3/5 py-3 items-center flex-col space-y-3 md:space-y-0 flex justify-evenly"
       >
         <div
-          class="flex max-w-5xl flex-col items-center w-full font-lexend justify-center space-y-8 lg:space-y-10"
+          class="flex flex-col w-full font-lexend justify-center space-y-8 lg:space-y-10"
         >
           <div
-            class="text-5xl w-full px-2 lg:text-7xl flex flex-col items-center justify-center space-y-2 text-white font-semibold"
+            class="text-5xl w-full text-start lg:text-6xl flex flex-col space-y-2 text-black font-semibold"
           >
-            <span class=""> Driving Ethiopia's </span>
-            <span class="relative whitespace-nowrap w-fit text-blue-600">
+            <span class=""> More Than Clients,</span>
+            <span class="relative whitespace-nowrap w-fit">
+              <span class="relative">We Become </span>
               <svg
                 aria-hidden="true"
-                class="absolute left-0 top-2/3 h-[0.58em] w-full fill-blue-500"
+                class="absolute right-0 top-[80%] h-[0.58em] w-5/12 fill-primary"
                 preserveAspectRatio="none"
                 viewBox="0 0 418 42"
               >
                 <path
                   d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"
                 ></path>
+                <span class="text-primary">Partners</span>
               </svg>
-              <span class="relative">Digital Future</span>
             </span>
           </div>
 
           <!--          Mobile version-->
-          <p class="lg:hidden text-gray-300 w-11/12">
+          <p class="lg:hidden font-light w-11/12">
             With creativity and technology, we craft software solutions that
             redefine possibilities. Join us in forging a digital ETHIOPIA!
           </p>
 
           <!--          Desktop version-->
           <p
-            class="max-w-3xl mx-auto mt-8 text-lg text-gray-400 font-light hidden md:inline-block"
+            class="w-11/12 text-lg mt-8 font-light text-gray-600 hidden md:inline-block"
           >
-            At Rigel, we blend innovation and technology to craft software
-            solutions that catalyze growth and drive progress. Join us on this
-            digital journey to transform ideas into realities and create a
-            technologically empowered ETHIOPIA!
+            We invest in understanding your vision and collaborate for
+            success.<br />
+            Together, we'll strategize and celebrate wins.
           </p>
         </div>
 
-        <div
-          class="flex flex-col space-y-3 md:space-y-0 md:flex-row max-w-5xl py-2 md:space-x-10 justify-center gap-3 mx-auto w-10/12"
-        >
-          <a
-            class="focus:outline-none inline-flex items-center font-semibold justify-center rounded-lg bg-blue-600 hover:bg-black hover:border-2 hover:border-primary text-white duration-200 lg:w-4/12 px-6 py-2 text-center w-full"
-            href="#about"
-            >Why Rigel?</a
+        <div class="flex w-full space-x-5">
+          <div
+            class="bg-primary rounded-lg cursor-pointer hover:scale-105 px-6 text-lg py-3 font-semibold text-white"
           >
+            Book a 15-min Call
+          </div>
+
           <a
-            class="focus:outline-none inline-flex items-center justify-center rounded-lg border-2 hover:text-white border-primary text-white font-bold duration-200 lg:w-4/12 px-6 py-2 hover:bg-primary cursor-pointer text-center w-full"
+            class="inline-flex w-fit items-center justify-center text-lg font-medium cursor-pointer hover:scale-105 hover:font-semibold hover:underline duration-200 px-4 py-1 text-center"
             href="#contact"
-            >Contact Us</a
-          >
+            >See Recent Works
+            <ChevronRightIcon class="w-6 pl-1" />
+          </a>
         </div>
       </div>
 
-      <!--      Desktop Version-->
-      <img
-        alt="Rigel Star"
-        class="hidden md:flex w-[42rem] h-[42rem] bg-primary/50 object-none rounded-full glow"
-        src="../assets/hero/rigel.png"
-      />
-    </div>
+      <div class="flex flex-col flex-wrap w-full space-y-3 py-2">
+        <div class="font-lexend font-light text-sm text-gray-600">
+          Working with
+        </div>
 
-    <div class="flex flex-col w-full space-y-5 py-2 bg-gray-900">
-      <div
-        class="font-lexend font-semibold italic md:not-italic md:text-2xl text-gray-400"
-      >
-        Trusted by these companies
+        <div ref="companyList" class="flex flex-wrap">
+          <company-item company="" label="Ankeboot Books"></company-item>
+          <company-item company="bo" label="ELSARO"></company-item>
+          <company-item company="bo" label="BB Trading"></company-item>
+          <!--          <company-item company="bo" label="Abadir SuperMarkets"></company-item>-->
+          <company-item company="bo" label="Maraki Fitness"></company-item>
+          <company-item company="bo" label="AIA"></company-item>
+          <company-item company="bo" label="Bios College"></company-item>
+          <company-item company="bo" label="Gurella Marketing"></company-item>
+          <company-item company="bo" label="And more"></company-item>
+        </div>
       </div>
-
-      <div
-        ref="companyList"
-        class="flex scrollbar-hide overflow-x-scroll snap-x snap-mandatory"
-      >
-        <company-item company="" label="Ankeboot Books"></company-item>
-        <company-item company="bo" label="Anbessa Betting"></company-item>
-        <company-item company="bo" label="BB Trading"></company-item>
-        <company-item company="bo" label="Bios College"></company-item>
-        <company-item company="bo" label="Maraki Fitness"></company-item>
-        <company-item company="bo" label="Alpha Betting"></company-item>
-        <company-item company="bo" label="AIA"></company-item>
-        <company-item company="bo" label="Universal PLC"></company-item>
-        <company-item company="bo" label="Agezhale Trading"></company-item>
-      </div>
+    </section>
+    <div class="w-5/12 flex min-h-full items-center">
+      <div class="bg-yellow-400 h-56 w-44 rounded-xl text-black">sdf</div>
     </div>
   </section>
 </template>
@@ -101,6 +90,8 @@
 import AOS from "aos";
 import CompanyItem from "../components/CompanyItem.vue";
 import { onMounted, ref } from "vue";
+import { ChevronRightIcon } from "@heroicons/vue/20/solid/index.js";
+import GridBackground from "../components/GridBackground.vue";
 
 AOS.init();
 
