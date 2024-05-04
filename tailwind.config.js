79908c3e09fa19b77daff5d3da8e012f184ef77e
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-05-04 08:55:16 +0300

    Add branding colors and poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,25 @@ export default {
       colors: {
         primary: "#2463eb",
         secondary: "#0C121C",
+        brand: {
+          50: "#eff6ff", // very light blue
+          100: "#dbeafe", // lighter blue
+          150: "#bfdbfe", // light blue
+          200: "#93c5fd", // less light blue
+          250: "#60a6fa", // muted blue
+          300: "#3b83f6", // medium blue
+          350: "#2463eb", // darkish blue
+          400: "#1d4fd8", // dark blue
+          450: "#1e41af", // darker blue
+          500: "#1e3a8a", // darkest blue
+        },
       },
       fontFamily: {
         sans: ["Inter"],
         lexend: ["Lexend"],
         basic: ["Lexend", "Noto Sans Ethiopic"],
+        poppins: ["Poppins"],
+        inter: ["Inter"],
       },
     },
   },
