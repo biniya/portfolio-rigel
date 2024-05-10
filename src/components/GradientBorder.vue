e2f2c948366c38189da47b9c08f3d3ae64537524
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-05-10 16:06:12 +0300

    Add gradient border box

---
+++ src/components/GradientBorder.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="gradientStyle" class="rounded-md w-full h-full p-1">
+    <div
+      class="bg-white h-full w-full flex flex-col items-center justify-evenly"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  gradientStyle: {
+    type: String,
+    default: "bg-gradient-to-r from-sky-500 via-purple-500 to-indigo-700",
+  },
+});
+</script>
+
+<style scoped></style>
